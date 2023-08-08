36b77da499627520d1bd5cfec1d0276a425a99a4
Sonward <86514352+SonwardGameMaker@users.noreply.github.com>
2023-08-08 18:29:31 +0300

    Added Window object

--- Assets/Scripts/Test/PathGrid/PathGrid.cs
+++ Assets/Scripts/Test/PathGrid/PathGrid.cs
@@ -75,6 +75,11 @@ public class PathGrid : MonoBehaviour
                 nodes[x, y] = new PathNode(this, x, y);
             }
         }
+
+        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        {
+
+        }
     }
 
 }
